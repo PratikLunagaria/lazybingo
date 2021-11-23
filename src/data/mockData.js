89d8b58a5720972913a927a6ba34255fad162a3f
Pratik Lunagaria <ramnikbhai_lunagaria@srmuniv.edu.in>
2021-11-24 03:54:56 +0530

    init

---
+++ src/data/mockData.js
@@ -0,0 +1,65 @@
+import clothes from './gifs/clothes.gif';
+import youtube from './gifs/youtube.gif';
+import internetsurf from './gifs/internetsurf.gif';
+import sleep from './gifs/sleep.gif';
+import homestuck from './gifs/homestuck.gif';
+import textnotif from './gifs/textnotif.gif';
+import dirtyclothes from './gifs/dirtyclothes.gif';
+import bingewatch from './gifs/bingewatch.gif';
+import toofar from './gifs/toofar.gif';
+import messyhair from './gifs/messyhair.gif';
+import noexercise from './gifs/noexercise.gif';
+import late from './gifs/late.gif';
+import freespace from './gifs/freespace.gif';
+import takeaway from './gifs/takeaway.gif';
+import food from './gifs/food.gif';
+import buyonline from './gifs/buyonline.gif';
+import sleepclass from './gifs/sleepclass.gif';
+import ignorecall from './gifs/ignorecall.gif';
+import oversleep from './gifs/oversleep.gif';
+import bored from './gifs/bored.gif';
+import breakfast from './gifs/breakfast.gif';
+import pyjamas from './gifs/pyjamas.gif';
+import chargerbed from './gifs/chargerbed.gif';
+import homework from './gifs/homework.gif';
+import messybun from './gifs/messybun.gif';
+
+export const mockData = [
+	{ title: 'uses the floor as a wardrobe', image: clothes },
+	{ title: 'watches YouTube all day', image: youtube },
+	{
+		title: 'obsessed with the internet',
+		image: internetsurf
+	},
+	{
+		title: 'sleeps a lot',
+		image: sleep
+	},
+	{ title: 'never goes outside', image: homestuck },
+	{
+		title: 'never replies to texts',
+		image: textnotif
+	},
+	{
+		title: 'wears the same outfit for 3 days in a row',
+		image: dirtyclothes
+	},
+	{ title: 'Bingewatch TV', image: bingewatch },
+	{ title: "It's too far", image: toofar },
+	{ title: 'messy hair', image: messyhair },
+	{ title: "doesn't exercise often", image: noexercise },
+	{ title: 'late for school', image: late },
+	{ title: 'free space', image: freespace },
+	{ title: 'orders takeaways often', image: takeaway },
+	{ title: 'food', image: food },
+	{ title: 'buys everything online', image: buyonline },
+	{ title: 'fails classes', image: sleepclass },
+	{ title: "doesn't answer phone calls", image: ignorecall },
+	{ title: 'oversleeps', image: oversleep },
+	{ title: 'sits in the same place/position for hours', image: bored },
+	{ title: 'breakfast for dinner', image: breakfast },
+	{ title: 'wears pyjamas all day', image: pyjamas },
+	{ title: 'has a charger by their bed', image: chargerbed },
+	{ title: 'never does homework', image: homework },
+	{ title: 'messy buns', image: messybun }
+];
